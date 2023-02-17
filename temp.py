1c1beffa3c03211d2c97b60786147f7bddcec134
a1084316 <a1084316@mail.nuk.edu.tw>
2023-02-17 19:55:42 +0800

    新增原始碼

---
+++ temp.py
@@ -0,0 +1,17 @@
+a=input("請輸入第一位學生的姓名:\n")
+b=input("請輸入第一位學生的成績:\n")
+c=input("請輸入第二位學生的姓名:\n")
+d=input("請輸入第二位學生的成績:\n")
+f=int(b)
+g=int(d)
+e=f+g
+print("姓名 成績")# -*- coding: utf-8 -*-
+print("%3s  %2d"%(a,f))
+print("%3s  %2d"%(c,g))
+print("成績總分為:%d"%(e))
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
